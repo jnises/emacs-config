4f151bdd5212b0ff99f4627460728d3e5c28afe1
Joel Nises <joel@nises.se>
2012-04-22 18:18:13 +0200

    added some utility functions

--- utils.el
+++ utils.el
@@ -20,6 +20,43 @@
         (insert " "))))
 
 
-;; (defun convert-to-dos ()
-;;   (interactive)
+(defun multi-union (seta setb)
+  "union of a multiset
+not sure if is correctly implemented"
+  (flet ((symbol< (a b) (string< (symbol-name a) (symbol-name b)))
+         (setsort (set)
+                  (sort set 'symbol<)))
+    (let ((sorteda (setsort seta))
+          (sortedb (setsort setb)))
+      (flet ((recur (seta setb result)
+                    (if seta
+                        (if setb
+                            (if (symbol< (car seta) (car setb))
+                                (recur (cdr seta) setb (cons (car seta) result))
+                              (if (symbol< (car setb) (car seta))
+                                  (recur seta (cdr setb) (cons (car setb) result))
+                                (recur (cdr seta) (cdr setb) result)))
+                          (append seta result))
+                      (append setb result))))
+        (recur sorteda sortedb '())))))
+
+
+(defun multi-intersection (seta setb)
+  "intersection of a multiset
+not sure if it is correctly implemented"
+  (flet ((symbol< (a b) (string< (symbol-name a) (symbol-name b)))
+         (setsort (set)
+                  (sort set 'symbol<)))
+    (let ((sorteda (setsort seta))
+          (sortedb (setsort setb)))
+      (flet ((recur (seta setb result)
+                    (if (and seta setb)
+                        (if (symbol< (car seta) (car setb))
+                            (recur (cdr seta) setb result)
+                          (if (symbol< (car setb) (car seta))
+                              (recur seta (cdr setb) result)
+                            (recur (cdr seta) (cdr setb) (cons (car seta) result))))
+                      result)))
+        (recur sorteda sortedb '())))))
+
   
