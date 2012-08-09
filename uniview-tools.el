0b238d8c409919f8b6910c45be1cb1df6d4a211a
Joel Nises <joel.nises@sciss.se>
2012-08-09 16:37:15 +0200

    removed useless stuff

--- uniview-tools.el
+++ uniview-tools.el
@@ -8,22 +8,10 @@
               (append a (list b))))
           files :initial-value '()))
 
-(setq uniview-sciss-profile-dir (concat (getenv "userprofile") "/sciss"))
+(defvar uniview-sciss-profile-dir (concat (getenv "userprofile") "/sciss") "where the uniview user date is located")
 
 (defun uniview-get-versions ()
   (uniview-filter-file-list (directory-files uniview-sciss-profile-dir)))
-
-(mapcar 'car 
-        (sort
-         (directory-files-and-attributes "c:/users/joel/sciss/dev/log" nil "^UniviewTheater\\.")
-         (lambda (a b)
-           (let ((adate (nth 5 (cdr a)))
-                 (bdate (nth 5 (cdr b))))
-             (or (> (car adate) (car bdate))
-                 (and (= (car adate) (car bdate))
-                      (> (cadr adate) (cadr bdate))))))))
-
-(nth 5 (file-attributes "uniview-tools.el"))
 
 (defun uniview-load-latest-log (version)
   "open a buffer for the latest uniview log for version (dev, master, ...)"
