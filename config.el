a5d757290b745e9c06df454c3a560b3b0d8d28c1
Joel Nises <joel.nises@sciss.se>
2015-09-01 14:43:17 +0200

    more helm stuff

--- config.el
+++ config.el
@@ -168,14 +168,23 @@
   (when (fboundp 'magit-find-file-ido)
     (global-set-key (kbd "C-c f") 'magit-find-file-ido)))
 
-(if (require 'helm-mode nil t)
+(if (and (require 'helm nil t) (require 'helm-config nil t))
     (progn
+      (setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+            helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+            ;;helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+            ;;helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+            helm-ff-file-name-history-use-recentf t
+            helm-mode-fuzzy-match t
+            helm-completion-in-region-fuzzy-match t
+            helm-M-x-fuzzy-match t
+            helm-buffers-fuzzy-matching t
+            helm-recentf-fuzzy-match t)
       (helm-mode 1)
-      (setq helm-mode-fuzzy-match t)
-      (setq helm-completion-in-region-fuzzy-match t)
-      (setq helm-M-x-fuzzy-match t)
       (global-set-key (kbd "C-x C-f") 'helm-find-files)
-      (global-set-key (kbd "M-x") 'helm-M-x))
+      (global-set-key (kbd "M-x") 'helm-M-x)
+      (global-set-key (kbd "C-x b") 'helm-mini)
+      (global-set-key (kbd "M-y") 'helm-show-kill-ring))
   ;; fall back to ido-mode if helm is not available
   (start-ido-mode))
 
