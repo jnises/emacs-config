548da17f0ec526175472a611bf4730efbb8f8a4a
Joel Nises <joel.nises@sciss.se>
2015-09-03 13:48:05 +0200

    helm tweaks

--- config.el
+++ config.el
@@ -172,16 +172,17 @@
 
 (if (and (require 'helm nil t) (require 'helm-config nil t))
     (progn
-      (setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
-            ;; helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
-            ;;helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
-            ;;helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
-            helm-ff-file-name-history-use-recentf t
-            helm-mode-fuzzy-match t
-            helm-completion-in-region-fuzzy-match t
-            helm-M-x-fuzzy-match t
-            helm-buffers-fuzzy-matching t
-            helm-recentf-fuzzy-match t)
+      ;; (setq ;;helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+      ;;  ;; helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+      ;;  ;;helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+      ;;  ;;helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+      ;;  ;;helm-ff-file-name-history-use-recentf t
+      ;;  ;;helm-mode-fuzzy-match t
+      ;;  ;;helm-completion-in-region-fuzzy-match t
+      ;;  ;;helm-M-x-fuzzy-match t
+      ;;  ;;helm-buffers-fuzzy-matching t
+      ;;  ;;helm-recentf-fuzzy-match t
+      ;;  )
       (helm-mode 1)
       (global-set-key (kbd "C-x C-f") 'helm-find-files)
       (global-set-key (kbd "M-x") 'helm-M-x)
