5dc87f3235717fc287d4f1d9dea647fb9dafd3fb
Joel Nises <joel.nises@sciss.se>
2015-09-08 18:12:15 +0200

    fastbuild-mode stuff,
    it doesn't work though

--- fastbuild-mode.el
+++ fastbuild-mode.el
@@ -1,8 +1,24 @@
-;;; -*- lexical-binding: t -*-
+(require 'js)
+(require 'noflet)
 
-(define-derived-mode fastbuild-mode c-mode "FB"
-  (setq comment-start ";")
-  (setq comment-end "")
+;; indentation (stolen from js.el
+
+(defun fb-indent-line ()
+  "Indent the current line as Fastbuild"
+  (interactive)
+  ;; override some js indentation rules
+  (let ((js--indent-operator-re "[-+*/%<>&^|?:]\\([^-+*/]\\|$\\)\\|!?=\\|")
+        (jsindentation 'js--proper-indentation))
+    (noflet ((js--proper-indentation (parse-status)
+                                     (if (= 0 (line-number-at-pos))
+                                         0
+                                       (funcall jsindentation parse-status)))))
+    (js-indent-line)))
+
+(define-derived-mode fastbuild-mode js-mode "FB"
+  (setq-local indent-line-function 'fb-indent-line)
+  (setq-local comment-start ";")
+  (setq-local comment-end "")
   (modify-syntax-entry ?\; "<" fastbuild-mode-syntax-table)
   (modify-syntax-entry ?/ "<12" fastbuild-mode-syntax-table)
   (modify-syntax-entry ?\n ">" fastbuild-mode-syntax-table)
