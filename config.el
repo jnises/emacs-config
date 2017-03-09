3fe3f0fe52c67c5fc80274e6e12d9e31e850eb65
joel nises <joel nises>
2017-03-09 13:36:43 +0100

    disable magit auto revert by default

--- config.el
+++ config.el
@@ -265,10 +265,12 @@ l is lab l, so the range is 0 to 100
   :ensure t
   :commands (magit-status magit-find-file-ido)
   :config
-  (when (string-equal system-type "windows-nt")
-    (let ((gitpath "c:/Program Files/Git/bin/git.exe"))
-      (if (file-exists-p gitpath)
-          (setq magit-git-executable gitpath)))))
+  (progn
+    (when (string-equal system-type "windows-nt")
+      (let ((gitpath "c:/Program Files/Git/bin/git.exe"))
+        (if (file-exists-p gitpath)
+            (setq magit-git-executable gitpath)))))
+  (setq magit-auto-revert-mode nil))
 
 ;; (use-package helm
 ;;   :ensure t)
@@ -394,16 +396,19 @@ l is lab l, so the range is 0 to 100
   ;;(setq gnuwin-path "c:/local/gnuwin32/bin")
   (setq cygwin-root-directory (if (file-exists-p "c:/cygwin64") "c:/cygwin64" "c:/cygwin"))
   (setq cygwin-bin-path (concat cygwin-root-directory "/bin"))
+  (setq cygwin-usr-bin-path (concat cygwin-root-directory "/usr/bin"))
   (setenv "PATH"
           (mapconcat 'identity
                      (mapcar (lambda (path) (replace-regexp-in-string "/" "\\\\" path)) (list ;;gnuwin-path
                                  ;;w32shell-cygwin-bin
                                  cygwin-bin-path
+                                 cygwin-usr-bin-path
                                  "C:/Program Files/Git/bin"
                                  (getenv "PATH")))
                      ";"))
 
   (add-to-list 'exec-path cygwin-bin-path)
+  (add-to-list 'exec-path cygwin-usr-bin-path)
   (add-to-list 'exec-path "C:/Program Files/Git/bin")
   ;; (set-variable 'find-program "find.exe")
   ;; (set-variable 'grep-program "grep.exe")
