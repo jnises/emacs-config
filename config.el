9804dcb5eb3fa290784758e90b8f5c55c20864eb
Joel Nises <joel@nises.se>
2017-07-08 10:08:45 +0200

    fixed booboo

--- config.el
+++ config.el
@@ -151,7 +151,7 @@
   (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
                       (not (gnutls-available-p))))
          (proto (if no-ssl "http" "https")))
-    (add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/") url) t)
+    (add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
     ;;(add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.milkbox.net/packages/") url) t)
     (when (< emacs-major-version 24)
       ;; For important compatibility libraries like cl-lib
