76ba64e279c84364387f3e6626a1a75e2d2bd300
Joel Nises <joel@nises.se>
2018-06-27 21:28:46 +0200

    cleanup

--- config.el
+++ config.el
@@ -145,7 +145,7 @@
   (global-unset-key [(control z)]))
 
 (defun find-windows-git-root ()
-  (if-let ((output (shell-command-to-string (concat "reg.exe query HKEY_LOCAL_MACHINE\\Software\\GitForWindows /v InstallPath")))
+  (if-let ((output (shell-command-to-string "reg.exe query HKEY_LOCAL_MACHINE\\Software\\GitForWindows /v InstallPath"))
            (regpos (string-match "REG_SZ" output))
            (pathstart (string-match "[^[:blank:]]" output (+ regpos (length "REG_SZ"))))
            (pathend (string-match "\n" output pathstart)))
