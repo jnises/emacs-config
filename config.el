db0264730e0d1c6116cbc8fa92253f0ee878595c
joel nises <joel.nises@gmail.com>
2021-12-07 18:07:29 +0100

    disabled melpa-stable and elpa

--- config.el
+++ config.el
@@ -329,13 +329,16 @@
 (unless (boundp 'package-archives)
   (setq package-archives '()))
 
+;; remove the elpa packages. they seem to have certificate issues or something
+(setq package-archives '())
+
 ;; better package repo
 ;; this seems to fix certificate issues
 (setq gnutls-algorithm-priority "normal:-vers-tls1.3")
 (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
                     (not (gnutls-available-p))))
        (proto (if no-ssl "http" "https")))
-  (add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  ;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
   (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
   (when (< emacs-major-version 24)
     ;; For important compatibility libraries like cl-lib
